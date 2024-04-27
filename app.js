cc7e1041633aebb09c03e0974a8f9e8421550b07
frasdor <frasunkiewicz.dorota@gmail.com>
2024-04-27 14:53:40 +0200

    Item List improvements
    I added a functionality to add item, DOM to handle what happens when mouse is over and out of the button. I added remove button with a function that removes items which were recenty clicked. Separate function gets index of the ckicked element within an array of list elements and stores it in a global varable.

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
-const itemInput = document.getElementById('itemInput');
-const addItemBtn = document.getElementById('addItemBtn');
-const itemList = document.getElementById('itemGrid');
+
 
 const recipesList = document.getElementById('recipesList');
 const resetBtn = document.getElementById('resetBtn');
 const searchRecipeBtn = document.getElementById('searchRecipeBtn');
 const inputField = document.getElementById('inputField');
+const itemInput = document.getElementById('itemInput');
+const addItemBtn = document.getElementById('addItemBtn');
+const removeButton = document.getElementById('removeButton');
+const itemGrid = document.getElementById('itemGrid');
 
 const APP_ID = '14494e99';
 const APP_KEY = '326753c7d2788295f7e0e911fcf5fdf7';
 let inputValue = '';
+let itemToRemove = -1; // variable to track selected item (-1 means no choice)
 
 
 inputField.addEventListener('change', (e) => {
     inputValue = e.target.value;
     console.log(inputValue);
 });
-
-addItemBtn.addEventListener('click', addItem);
 
 searchRecipeBtn.addEventListener('click', (e) => {
     e.preventDefault();
@@ -33,15 +34,7 @@ resetBtn.addEventListener('click', () => {
 }
 );
 
-function addItem() {
-    const newItemText = itemInput.value.trim();
-    if (newItemText !== '') {
-        const li = document.createElement('li');
-        li.textContent = newItemText;
-        itemList.appendChild(li);
-        itemInput.value = '';
-    }
-}
+
 
 function getRecipes() {
     fetch(`https://api.edamam.com/search?q=${inputValue}&app_id=${APP_ID}&app_key=${APP_KEY}`)
@@ -71,4 +64,61 @@ function getRecipes() {
 resetBtn.addEventListener('click', () => {
     recipesList.innerHTML = '';
     inputField.value = '';
+});
+
+
+
+  itemGrid.addEventListener('click', function(event) {
+    //let target= event.target;
+    //let li = target// target.closest('li');  // get reference by using closest
+    
+    let li = event.target;
+    //console.log(li);
+    let ulItems = Array.from( li.closest('ul').children ); // get array
+    let index = ulItems.indexOf( li ); 
+    itemToRemove = index; // itemToRemove gets index of the clicked element  
+});
+    
+ // function to add items to the list
+    function addItemToList(text) {
+    const listItem = document.createElement('li');
+    listItem.textContent = text;
+
+    // what happens when mouse is over the button
+    listItem.addEventListener('mouseover', function() { 
+        listItem.style.backgroundColor = '#333';
+        listItem.style.fontWeight = 'bold';
+        listItem.style.transition = '.5s';
+        
+    });
+
+    //what happens when mouse is out the button
+    listItem.addEventListener('mouseout', function() {
+        listItem.style.backgroundColor = '';
+        listItem.style.fontWeight = '';
+    });
+
+   // add listItem to itemGrid as a child
+    itemGrid.appendChild(listItem);
+}
+
+// Add button
+addItemBtn.addEventListener('click', function() {
+    const text = itemInput.value.trim();
+
+    // check if text field is empty
+    if (text) {
+        addItemToList(text);
+        itemInput.value = ''; // clear the text field
+    }
+});
+
+// Remove button
+removeButton.addEventListener('click', function() {
+    const listItems = itemGrid.getElementsByTagName('li');
+    
+    if (listItems.length > 0 && itemToRemove>-1) {
+        itemGrid.removeChild(listItems[itemToRemove]); // remove most recently clicked
+        itemToRemove = -1;
+    }
 });
